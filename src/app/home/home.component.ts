70b3e0dd6d1cfa465249e5e111f798a2cbd2c860
Mykyta <arm454h@gmail.com>
2022-01-09 17:21:15 +0100

    Improve home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+import values from '../../values';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  shouldAnimateMoveDownBtn = true;
+
+  constructor(private viewportScroller: ViewportScroller) {
+    this.viewportScroller.setOffset([0, values.HEADER_CONTENT_OFFSET_PX]);
+  }
 
   ngOnInit(): void {
+  }
+
+  onMoveDownClick(event: any) {
+    this.shouldAnimateMoveDownBtn = false;
+
+    this.viewportScroller.scrollToAnchor('moveDownScrollHere');
   }
 
 }
---
+++ src/values.ts
@@ -0,0 +1,6 @@
+
+const values = {
+    HEADER_HEIGHT_PX: 60,
+    HEADER_CONTENT_OFFSET_PX: 82,
+};
+export default values; 
